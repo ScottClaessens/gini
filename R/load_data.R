acb69e4db3f418d0f800c4db091fa007d6bde785
Scott Claessens <scott.claessens@gmail.com>
2026-06-17 10:39:37 +0100

    Refresh project

---
+++ R/load_data.R
@@ -0,0 +1,23 @@
+#' Load data on archaeological sites
+#'
+#' @param data_file File path to dataset
+#'
+#' @returns A tibble
+#'
+load_data <- function(data_file) {
+  data_file |>
+    read_csv(show_col_types = FALSE) |>
+    transmute(
+      id         = `...1`,
+      site       = Site,
+      bigregion  = Bigregion,
+      region     = Region,
+      subregion  = Subregion,
+      subarea    = Subarea,
+      latitude   = Latitude,
+      longitude  = Longitude,
+      date       = Date,
+      gini       = Gini,
+      polity_pop = PolityPop
+    )
+}
--- _targets.R
+++ _targets.R
@@ -2,7 +2,7 @@ library(stantargets)
 library(targets)
 library(tarchetypes)
 
-tar_option_set(packages = c("cmdstanr", "loo", "patchwork", "tidyverse"))
+tar_option_set(packages = c("cmdstanr", "tidyverse"))
 tar_source()
 
 list(
@@ -12,92 +12,6 @@ list(
   # ─────────────────────────────────────────
   
   tar_target(data_file, "data/SiteGiniLevel.csv", format = "file"),
-  tar_target(data, read_csv(data_file, show_col_types = FALSE)),
-  
-  # ─────────────────────────────────────────
-  # Plot Gini and population size over time
-  # ─────────────────────────────────────────
-  
-  tar_target(plot_gini_global, plot_gini_time(data)),
-  tar_target(plot_gini_by_region, plot_gini_time(data, split_by_region = TRUE)),
-  tar_target(plot_population_global, plot_population_time(data)),
-  tar_target(
-    plot_population_by_region,
-    plot_population_time(data, split_by_region = TRUE)
-  ),
-  
-  # ─────────────────────────────────────────
-  # Baseline model
-  # ─────────────────────────────────────────
-  
-  # prior predictive check
-  tar_stan_mcmc(
-    name = prior_baseline,
-    stan_files = "stan/model_baseline.stan",
-    data = get_stan_data_list(data, prior_only = TRUE),
-    parallel_chains = 4,
-    seed = 1
-  ),
-  tar_target(
-    prior_check_baseline,
-    plot_predictive_check(data, prior_baseline_draws_model_baseline,
-                          filename = "plots/prior_check_baseline.pdf")
-  ),
-  
-  # fit model
-  tar_stan_mcmc(
-    name = fit_baseline,
-    stan_files = "stan/model_baseline.stan",
-    data = get_stan_data_list(data),
-    parallel_chains = 4,
-    seed = 1
-  ),
-  
-  # loo-cv
-  tar_target(loo_baseline, fit_baseline_mcmc_model_baseline$loo()),
-  
-  # posterior predictive check
-  tar_target(
-    pred_check_baseline,
-    plot_predictive_check(data, fit_baseline_draws_model_baseline,
-                          filename = "plots/pred_check_baseline.pdf")
-  ),
-  
-  # ─────────────────────────────────────────
-  # SDE model
-  # ─────────────────────────────────────────
-  
-  # prior predictive check
-  tar_stan_mcmc(
-    name = prior_sde,
-    stan_files = "stan/model_sde.stan",
-    data = get_stan_data_list(data, prior_only = TRUE),
-    parallel_chains = 4,
-    seed = 1
-  ),
-  tar_target(
-    prior_check_sde,
-    plot_predictive_check(data, prior_sde_draws_model_sde,
-                          filename = "plots/prior_check_sde.pdf")
-  ),
-  
-  # fit model
-  tar_stan_mcmc(
-    name = fit_sde,
-    stan_files = "stan/model_sde.stan",
-    data = get_stan_data_list(data),
-    parallel_chains = 4,
-    seed = 1
-  ),
-  
-  # loo-cv
-  tar_target(loo_sde, fit_sde_mcmc_model_sde$loo()),
-  
-  # posterior predictive check
-  tar_target(
-    pred_check_sde,
-    plot_predictive_check(data, fit_sde_draws_model_sde,
-                          filename = "plots/pred_check_sde.pdf")
-  )
+  tar_target(data, load_data(data_file))
   
 )
