3fa5f8a6b57a7c21fe46eb21388418a6a97f81bc
Scott Claessens <scott.claessens@gmail.com>
2026-06-26 16:39:43 +0100

    Update HYDE dataset to include full time series for all lon-lat coordinates

--- data/hyde/download_hyde_data.R
+++ data/hyde/download_hyde_data.R
@@ -3,24 +3,23 @@
 # Author: Scott Claessens
 #
 # This code loops over time slices in the HYDE dataset. For each time slice, the
-# code downloads raster data from HYDE and links it to the Gini dataset. If 
-# there are no archaeological sites in the time slices, the loop skips the
-# download. The resulting data file is written to hyde.csv in the data/hyde/ 
-# directory.
+# code downloads raster data from HYDE and links it to longitude-latitude 
+# coordinates from the Gini dataset. The resulting data file is written to 
+# hyde.csv in the data/hyde/ directory.
 #
-# Estimated run time on a MacBook Pro is around 10 minutes.
+# Estimated run time on a MacBook Pro is around 20 minutes.
 #
 
-library(raster)     # v1.9-27
+library(terra)      # v1.9-27
 library(tidyverse)  # v2.0.0
 
-# get time slices in hyde dataset
+# get time slices in hyde dataset up to 1980 (maximum date in gini dataset)
 time_slices <- 
   c(
     seq(from = -10000, to = 0,    by = 1000),
     seq(from = 100,    to = 1700, by = 100),
     seq(from = 1710,   to = 1950, by = 10),
-    seq(from = 1951,   to = 2025, by = 1)
+    seq(from = 1951,   to = 1980, by = 1)
   )
 
 # get names of time slices
@@ -32,99 +31,87 @@ names(time_slices) <-
 # load gini dataset for linking
 gini <- read_csv("data/gini/SiteGiniLevel.csv", show_col_types = FALSE)
 
+# drop sites without longitude-latitude coordinates
+gini <- drop_na(gini, c(Longitude, Latitude))
+
+# get unique longitude-latitude coordinates
+unique_lon_lat <- unique(gini[, c("Longitude", "Latitude")])
+
+# get lon-lat coordinates as points
+points <- vect(
+  unique_lon_lat,
+  geom = c("Longitude", "Latitude"),
+  crs = "EPSG:4326"
+)
+
 # initialise hyde dataset
-out <- tibble() 
+out <- tibble()
 
 # loop over time slices
-for (i in 1:(length(time_slices) - 1)) {
+for (i in 1:length(time_slices)) {
   
-  # retain archaeological sites in this time slice
-  d <- filter(gini, Date >= time_slices[i] & Date < time_slices[i + 1])
+  # print progress message
+  message("Downloading HYDE data for ", names(time_slices)[i])
   
-  # if there are archaeological sites in this time slice, load hyde data
-  if (nrow(d) > 0) {
+  # initialise time slice data
+  d <- bind_cols(date = time_slices[i], unique_lon_lat)
+  
+  # loop over different variables to extract
+  for (variable in c("cropland", "popc")) {
     
-    # print progress message
-    message("Downloading HYDE data for ", names(time_slices)[i])
+    # get url of zip file for data
+    url <- paste0(
+      "https://geo.public.data.uu.nl/vault-hyde/",
+      "hyde35_c9_apr2025%5B1749214444%5D/original/",
+      "gbc2025_7apr_base/zip/",
+      names(time_slices)[i],
+      "_",
+      ifelse(variable == "cropland", "lu", "pop"),
+      ".zip"
+    )
     
-    # loop over different variables to extract
-    for (variable in c("cropland", "popc")) {
-      
-      # get url of zip file for data
-      url <- paste0(
-        "https://geo.public.data.uu.nl/vault-hyde/",
-        "hyde35_c9_apr2025%5B1749214444%5D/original/",
-        "gbc2025_7apr_base/zip/",
-        names(time_slices)[i],
-        "_",
-        ifelse(variable == "cropland", "lu", "pop"),
-        ".zip"
-      )
-      
-      # download zip file into data/hyde/ directory
-      zip_file <- tempfile()
-      download.file(url, zip_file, quiet = TRUE)
-      
-      # identify raster file
-      raster_file <- paste0(
-        variable,
-        ifelse(variable == "cropland", "", "_"),
-        names(time_slices)[i],
-        ".asc"
-      )
-      
-      # unzip into hyde directory
-      unzip(
-        zipfile = zip_file,
-        files = raster_file,
-        exdir = "data/hyde"
-      )
-      
-      # get lon-lat points
-      points <- vect(
-        d[, c("Longitude", "Latitude")],
-        geom = c("Longitude", "Latitude"),
-        crs = "EPSG:4326"
-      )
-      
-      # load raster file
-      r <- rast(paste0("data/hyde/", raster_file))
-      
-      # project to a metric CRS
-      points_m <- project(points, "EPSG:3857")
-      r_m <- project(r, "EPSG:3857")
-      
-      # create 5km buffers around site lon-lat points
-      buffers <- buffer(points_m, width = 5000)
-      
-      # extract data from raster for these sites
-      d[[variable]] <- extract(r_m, buffers, fun = mean, na.rm = TRUE)[, 2]
-      
-      # clean up
-      file.remove(paste0("data/hyde/", raster_file))
-      
-    }
+    # download zip file into data/hyde/ directory
+    zip_file <- tempfile()
+    download.file(url, zip_file, quiet = TRUE)
     
-    # bind rows
-    out <- bind_rows(out, d)
+    # identify raster file
+    raster_file <- paste0(
+      variable,
+      ifelse(variable == "cropland", "", "_"),
+      names(time_slices)[i],
+      ".asc"
+    )
     
-  } else {
+    # unzip into hyde directory
+    unzip(
+      zipfile = zip_file,
+      files = raster_file,
+      exdir = "data/hyde"
+    )
     
-    # print skip message
-    message("Skipping HYDE data for ", names(time_slices)[i])
+    # load raster file
+    r <- rast(paste0("data/hyde/", raster_file))
+    
+    # extract data from raster for lon-lat points
+    d[[variable]] <- extract(r, points)[, 2]
+    
+    # clean up
+    file.remove(paste0("data/hyde/", raster_file))
     
   }
+  
+  # bind rows
+  out <- bind_rows(out, d)
   
 }
 
 # write to csv
 out |>
   transmute(
-    site      = Site,
+    date      = date,
     longitude = Longitude,
     latitude  = Latitude,
-    date      = Date,
-    cropland  = cropland,
-    pop_size  = popc
+    cropland  = round(cropland, 2),
+    pop_size  = round(popc, 2)
   ) |>
   write_csv("data/hyde/hyde.csv")
