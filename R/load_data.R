d4c4a1d130facfd0f75f3259c464bc6e73d7a173
Scott Claessens <scott.claessens@gmail.com>
2026-06-26 18:26:30 +0100

    Load full HYDE time-series data

--- R/load_data.R
+++ R/load_data.R
@@ -13,28 +13,54 @@ load_data <- function(file_gini, file_hyde) {
     read_csv(show_col_types = FALSE) |>
     transmute(
       id         = `...1`,
-      site       = Site,
-      bigregion  = Bigregion,
       region     = Region,
       subregion  = Subregion,
-      subarea    = Subarea,
       latitude   = Latitude,
       longitude  = Longitude,
       date       = Date,
       gini       = Gini
     ) |>
-    # drop sites with no date information
-    drop_na(date)
+    # drop sites with no information on dates or longitude/latitude
+    drop_na(c(date, longitude, latitude)) |>
+    # fix some subregions to ensure one subregion per lon-lat point
+    mutate(
+      subregion = paste(region, subregion),
+      subregion = ifelse(id == 230, "W Europe Switzerland", subregion),
+      subregion = ifelse(id == 664, "Great Britain Central Belt", subregion),
+      subregion = ifelse(id == 902, "Great Britain East", subregion),
+      subregion = ifelse(id == 916, "SE Europe Bulgaria", subregion)
+    )
   
   # load HYDE data
   hyde <- read_csv(file_hyde, show_col_types = FALSE)
   
-  # join datasets and return
-  left_join(gini, hyde, by = c("site", "longitude", "latitude", "date")) |>
-    # drop sites with no HYDE data
-    drop_na(c(cropland, pop_size)) |>
-    # normalise dates into 0-1 range
-    mutate(t = (date - min(date)) / (max(date) - min(date))) |>
-    arrange(t)
+  # get unique lon-lat coordinates and subregions
+  coords <- 
+    dplyr::select(gini, longitude, latitude, subregion) |>
+    unique()
+  
+  # summarise hyde data by subregion
+  d <-
+    left_join(
+      hyde,
+      coords, 
+      by = c("longitude", "latitude")
+    ) |>
+    # summarise by subregion
+    group_by(date, subregion) |>
+    summarise(
+      across(longitude:pop_size, function(x) mean(x, na.rm = TRUE)),
+      .groups = "drop"
+    ) |>
+    arrange(subregion, date)
+  
+  # add gini data and return
+  bind_rows(d, dplyr::select(gini, date, subregion, gini)) |>
+    fill(
+      c(longitude, latitude),
+      .by = "subregion",
+      .direction = "updown"
+    ) |>
+    arrange(subregion, date)
   
 }
--- _targets.R
+++ _targets.R
@@ -15,35 +15,6 @@ list(
   
   tar_target(file_gini, "data/gini/SiteGiniLevel.csv", format = "file"),
   tar_target(file_hyde, "data/hyde/hyde.csv", format = "file"),
-  tar_target(data, load_data(file_gini, file_hyde)),
-  
-  # ─────────────────────────────────────────
-  # Fit Stan model
-  # ─────────────────────────────────────────
-  
-  tar_stan_mcmc(
-    fit,
-    stan_files = "stan/model.stan",
-    data = get_data_list(data),
-    parallel_chains = 4,
-    seed = 1
-  ),
-  
-  # ─────────────────────────────────────────
-  # Plot model predictions and checks
-  # ─────────────────────────────────────────
-  
-  tar_target(
-    plot_pred_global,
-    plot_global_predictions(data, fit_draws_model)
-  ),
-  tar_target(
-    plot_pred_regional,
-    plot_regional_predictions(data, fit_draws_model)
-  ),
-  tar_target(
-    plot_pp_check,
-    plot_predictive_check(data, fit_draws_model)
-  )
+  tar_target(data, load_data(file_gini, file_hyde))
 
 )
